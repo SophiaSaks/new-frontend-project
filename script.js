6d496cb6360622de4e3bf5176ab8be20389b195e
Sophia S <sophia.saks@live.se>
2024-12-13 23:45:48 +0100

    styling and some responsiveness

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ let humidity = document.querySelector('.humidity');
 let pressure = document.querySelector('.pressure');
 let form = document.querySelector('form');
 let figcaption = document.getElementById('figcaption');
+let main = document.querySelector('main');
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
@@ -24,7 +25,7 @@ async function checkWeather() {
     var data = await response.json();
     if(data.cod == 200){
         city.querySelector('figcaption').innerText = data.name;
-        city.querySelector('img').src = `https://flagsapi.com/${data.sys.country}/shiny/32.png`;
+        city.querySelector('img').src = `https://flagsapi.com/${data.sys.country}/flat/32.png`;
 
         temperature.querySelector('img').src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`;
         temperature.querySelector('span').innerText = Math.round(data.main.temp);
@@ -34,5 +35,13 @@ async function checkWeather() {
         clouds.innerText = data.clouds.all;
         humidity.innerText = data.main.humidity;
         pressure.innerText = data.main.pressure;
+    }else{
+        main.classList.add('error');
+        setTimeout(() => {
+            main.classList.remove('error')
+        }, 1000);
     }
+
+    valueSearch.value = '';
 }
+
