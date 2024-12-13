ab5d5713cb7c4beccbd19014ed97891a46b5c702
Sophia S <sophia.saks@live.se>
2024-12-13 13:03:51 +0100

    make sure weather app takes data from api

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 
 let valueSearch = document.getElementById('valueSearch');
-let city = document.querySelector('city');
+let city = document.getElementById('city');
 let temperature = document.querySelector('.temperature');
 let description = document.querySelector('.description');
 let clouds = document.querySelector('.clouds');
 let humidity = document.querySelector('.humidity');
 let pressure = document.querySelector('.pressure');
 let form = document.querySelector('form');
+let figcaption = document.getElementById('figcaption');
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
@@ -18,21 +19,20 @@ form.addEventListener('submit', (event) => {
 const apiKey = "5934d7c41c30f5c76770d6a905d6848e";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&apikey=" + apiKey;
 
-// const searchWeather = () => {
-//     fetch(apiUrl + '&q=' + valueSearch.value)
-//     .then(res => res.json)
-//     .then(data => console.log(data))
-// }
 async function checkWeather() {
     const response = await fetch(apiUrl + "&q=" + valueSearch.value);
     var data = await response.json();
     if(data.cod == 200){
         city.querySelector('figcaption').innerText = data.name;
-        city.querySelector('img').src = `"https://flagsapi.com/${data.sys.country}/shiny/32.png"`;
+        city.querySelector('img').src = `https://flagsapi.com/${data.sys.country}/shiny/32.png`;
+
+        temperature.querySelector('img').src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`;
+        temperature.querySelector('span').innerText = Math.round(data.main.temp);
+
+        description.innerText = data.weather[0].description;
+
+        clouds.innerText = data.clouds.all;
+        humidity.innerText = data.main.humidity;
+        pressure.innerText = data.main.pressure;
     }
-
-    console.log(data)
-    console.log(data.name)
-
-
 }
